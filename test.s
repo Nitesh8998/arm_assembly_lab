93cd6b28dc903de3d92d7472e10ec2451e144cba
Hariprasad Boga <hboga@qti.qualcomm.com>
2019-09-08 23:02:39 +0530

    other approaches to get absolute value (in test.s)

--- test.s
+++ test.s
@@ -1,1 +1,11 @@
 @test
+
+Approach 1:
+ASR R1, R0, #31         ; 1 cycle
+ADD R0, R0, R1          ; 1 cycle
+EOR R0, R0, R1          ; 1 cycle
+                        ; total: 3 cycles
+Approach 2:
+ADD R1, R0, R0, ASR #31 ; 1 cycle
+EOR R0, R1, R0, ASR #31 ; 1 cycle
+                        ; total: 2 cycles
