1cffb1bf9779828236960d47150c84abb396567c
Hariprasad Boga <hboga@qti.qualcomm.com>
2019-09-08 23:47:38 +0530

    documentation for division program is updated

--- test.s
+++ test.s
@@ -9,3 +9,16 @@ Approach 2:
 ADD R1, R0, R0, ASR #31 ; 1 cycle
 EOR R0, R1, R0, ASR #31 ; 1 cycle
                         ; total: 2 cycles
+
+
+Documentation for Division program:
+To perform r2/r3:
+Repeatedly perform r3<<32, r3<<31... and r2<<1...
+While doing this repetetive LSL, compare the values with r2 and 
+substract the shifted value from r2 and decrement r2 accordinly.
+After subtraction, update the quotient as per the shift made with 1 (1 << shifts count ) made to compute LSL value.
+
+By adding all the shift based values using left shift with value 1, quotient can be computed.
+
+These shifts and comparisons will be continued till shift count reaches 0.
+or till dividend becomes smaller than divisor. 
